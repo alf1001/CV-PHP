849618fdfb07dcc40e2c586e3b4481333ae0b3f2
Ahmad Alfin Nur Hakim <ahmadalfin130804@gmail.com>
2023-11-11 10:06:22 +0700

    update

--- cv/index.php
+++ cv/index.php
@@ -21,65 +21,56 @@ $data = mysqli_fetch_array($result);
   <title>Curriculum Vitae</title>
 </head>
 
-<body class="p-3">
-  <nav class="navbar sticky-top bg-body-tertiary biru">
-    <div class="container" >
-      <h1>Curriculum Vitae</h1>
-      <a class="navbar-brand" href="admin.php">Update</a>
-    </div>
-  </nav>
-  <div class="card" style="background-color: #465967; height:85vh">
-    <div class="p-3">
-      <img src="<?php echo $data['foto_path']; ?>" alt="Foto Profil">
-      <div class="card-body">
-        <h1 class="card-title"><?php echo $data['nama']; ?></h1>
-        <p class="card-text"><?php echo $data['alamat']; ?></p>
-        <p class="card-text"><?php echo $data['telepon']; ?></p>
-        <p class="card-text"><?php echo $data['email']; ?></p>
-        <p class="card-text">
-          <i class="fab fa-github"></i>
-          <?php echo $data['web']; ?></p>
-        <h2 class="pt-3">Pendidikan</h2>
-        <p class="card-text"><?php echo $data['pendidikan']; ?></p>
-        <h2 class="pt-3">Pengalaman Kerja</h2>
-        <p class="card-text"><?php echo $data['pengalaman_kerja']; ?></p>
-        <h2 class="pt-3">Keterampilan</h2>
-        <p class="card-text"><?php echo $data['keterampilan']; ?></p>
+<body>
+  <div id="container">
+    <div class="row" id="header">
+      <div class="col-md-6" id="column">
+        <img src="<?php echo $data['foto_path']; ?>" alt="Foto Profile" id="image" class="">
       </div>
+      <div class="col-md-6">
+        <h1 class="nombre py-4 pt-5"><?php echo $data['nama']; ?></h1>
+        <p id="line"></p>
+        <!-- <h2 id="description">Fullstack Developer</h2> -->
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 pt-4">
+        <h4 id="Alamat">Alamat</h4>
+        <p><?php echo $data['alamat']; ?></p>
+      </div>
+      <div class="col-md-12">
+        <h4 id="Telepon">Telepon</h4>
+        <p><?php echo $data['telepon']; ?></p>
+      </div>
+      <div class="col-md-12">
+        <h4 id="Email">Email</h4>
+        <p><?php echo $data['email']; ?></p>
+      </div>
+      <div class="col-md-12">
+        <h4 id="Web">WEB</h4>
+        <p>
+          <i class="fab fa-github"></i>
+          <?php echo $data['web']; ?>
+        </p>
+      </div>
+      <div class="col-md-12">
+        <h4 id="Pendidikan">Pendidikan</h4>
+        <p><?php echo $data['pendidikan'];?></p>
+      </div>
+      <div class="col-md-12">
+        <h4 id="Pengalaman-kerja">Pengalaman Kerja</h4>
+        <p><?php echo $data['pengalaman_kerja']; ?></p>
+      </div>
+      <div class="col-md-12">
+        <h4 id="Keterampilan">Keterampilan</h4>
+        <?php echo $data['keterampilan']; ?>
+      </div>
+    </div>
+    <div>
+      <a class="btn btn-warning" href="admin.php">Update</a>
     </div>
   </div>
-  <!-- Tampilkan komentar -->
-  <!-- <nav class="navbar sticky-top bg-body-tertiary biru">
-    <div class="container-fluid">
-      <h1>Komentar</h1>
-    </div>
-  </nav>
-  <div class="card">
-    <div class="p-3">
-      <div id="comments">
-        <?php
-        include 'config.php';
 
-        $cvId = 1; 
-        $query = "SELECT * FROM comments WHERE cv_id = $cvId";
-        $result = mysqli_query($conn, $query);
-
-        if ($result && mysqli_num_rows($result) > 0) {
-          while ($comment = mysqli_fetch_assoc($result)) {
-            echo '<div class="comment">' . $comment['comment_text'] . '</div>';
-          }
-        } else {
-          echo 'Belum ada komentar.';
-        }
-
-        mysqli_close($conn);
-        ?>
-      </div>
-      <label for="commentInput" class="form-label">Tambahkan Komentar</label>
-      <textarea class="form-control" id="commentInput" name="comment" rows="3" placeholder="Tambahkan komentar..."></textarea>
-      <button class="btn btn-primary" onclick="addComment()">Tambah Komentar</button>
-    </div>
-  </div> -->
 
 </body>
 
